139004cc8cc15aec2a1ccdfe7a3156cec9f13e96
renlm <renlmer@163.com>
2022-02-26 17:35:26 +0800

    Init

--- src/main/java/cn/renlm/graph/VirtualPhoneApplication.java
+++ src/main/java/cn/renlm/graph/VirtualPhoneApplication.java
@@ -1,4 +1,4 @@
-package com.lei.du;
+package cn.renlm.graph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/cn/renlm/graph/GeneratorTest.java
+++ src/test/java/cn/renlm/graph/GeneratorTest.java
@@ -1,4 +1,4 @@
-package com.lei.du;
+package cn.renlm.graph;
 
 import org.junit.jupiter.api.Test;
 
