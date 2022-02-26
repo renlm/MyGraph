397d04c488d5312abbe3f3766a41cfeabdac515d
renlm <renlmer@163.com>
2022-02-26 21:06:15 +0800

    Init

--- src/main/java/cn/renlm/graph/controller/GraphController.java
+++ src/main/java/cn/renlm/graph/controller/GraphController.java
@@ -1,13 +1,20 @@
 package cn.renlm.graph.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+
+import cn.hutool.json.JSONUtil;
 import cn.renlm.graph.common.Result;
 import cn.renlm.graph.dto.GraphDto;
+import cn.renlm.graph.entity.Graph;
+import cn.renlm.graph.service.IGraphService;
 
 /**
  * 图形设计
@@ -18,6 +25,23 @@ import cn.renlm.graph.dto.GraphDto;
 @Controller
 @RequestMapping("/graph")
 public class GraphController {
+
+	@Resource
+	private IGraphService iGraphService;
+
+	/**
+	 * 编辑器
+	 * 
+	 * @param model
+	 * @param uuid
+	 * @return
+	 */
+	@RequestMapping("/editor")
+	public String editor(ModelMap model, String uuid) {
+		Graph graph = iGraphService.getOne(Wrappers.<Graph>lambdaQuery().eq(Graph::getUuid, uuid));
+		model.put("graphJson", JSONUtil.toJsonStr(graph));
+		return "graph/editor";
+	}
 
 	/**
 	 * 保存编辑器
