387bdae30f3913647358d4f2c241c0cefee132dc
renlm <renlmer@163.com>
2022-03-06 19:27:23 +0800

    Init

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,6 +3,30 @@
  */
 (function($) {
 	$.extend({
+		/***
+		 * Toastr通知（成功）
+		 */
+		toastrSuccess: function (message) {
+			toastr.options = {
+				closeButton: true,
+				debug: false,
+				progressBar: true,
+				positionClass: "toast-top-right",
+				onclick: null,
+				showDuration: "400",
+				hideDuration: "1000",
+				timeOut: "7000",
+				extendedTimeOut: "1000",
+				showEasing: "swing",
+				hideEasing: "linear",
+				showMethod: "fadeIn",
+				hideMethod: "fadeOut"
+			};
+			toastr.success(message);
+		},
+		/***
+		 * 修改密码
+		 */
 		editPwd: function() {
 			window.editPwdLayerIndex = layer.open({
 				type: 2, 
