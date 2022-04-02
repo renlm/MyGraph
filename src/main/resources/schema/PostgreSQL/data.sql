681c9f3d6e53a805578296fc97bd85f3d956ff10
任黎明(10021235) <renlm-a@glodon.com>
2022-04-02 16:17:02 +0800

    sss

--- src/main/resources/schema/PostgreSQL/data.sql
+++ src/main/resources/schema/PostgreSQL/data.sql
@@ -28,14 +28,18 @@ INSERT INTO graph (
 	category_code,
 	category_name,
 	is_public,
+	created_at,
 	creator_user_id,
-	creator_nickname
+	creator_nickname,
+	updated_at
 ) VALUES (
 	'198124BBCF284A40BB24CA315A7B8E36', 
 	'演示DEMO',
 	'ER',
 	'ER模型',
 	true,
+	now(),
 	'04AB1FE543D74862A1B35676C7BBE61B',
-	'任黎明'
+	'任黎明',
+	now()
 );
