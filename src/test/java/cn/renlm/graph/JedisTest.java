45c217f445d0fb5847a4fc18d8a85d10ebe5a0f6
任黎明(10021235) <renlm-a@glodon.com>
2022-04-14 16:27:24 +0800

    s

--- src/test/java/cn/renlm/graph/JedisTest.java
+++ src/test/java/cn/renlm/graph/JedisTest.java
@@ -1,8 +1,13 @@
 package cn.renlm.graph;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.junit.jupiter.api.Test;
 
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
 import cn.hutool.db.nosql.redis.RedisDS;
 import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
@@ -23,6 +28,16 @@ public class JedisTest {
 	@Test
 	public void setex() {
 		String reply = jedis.setex(key, 10L, IdUtil.getSnowflakeNextIdStr());
-		log.info(reply);
+		log.info("保存数据：{}", reply);
+		AtomicInteger i = new AtomicInteger();
+		CronUtil.schedule("*/1 * * * * ?", new Task() {
+			@Override
+			public void execute() {
+				log.info("{}.获取数据：{}", i.incrementAndGet(), jedis.get(key));
+			}
+		});
+		CronUtil.setMatchSecond(true);
+		CronUtil.start();
+		ThreadUtil.safeSleep(10000);
 	}
 }
