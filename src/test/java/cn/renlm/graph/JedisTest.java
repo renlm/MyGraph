521729fa4b77eed4c26052ad3aca57135848449e
任黎明(10021235) <renlm-a@glodon.com>
2022-04-18 10:58:19 +0800

    Jedis工具

--- src/test/java/cn/renlm/graph/JedisTest.java
+++ src/test/java/cn/renlm/graph/JedisTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.cron.CronUtil;
@@ -25,6 +26,9 @@ public class JedisTest {
 
 	private Jedis jedis = RedisDS.create().getJedis();
 
+	/**
+	 * 为指定的 key 设置值及其过期时间。如果 key 已经存在，SETEX 命令将会替换旧的值
+	 */
 	@Test
 	public void setex() {
 		String reply = jedis.setex(key, 5L, IdUtil.getSnowflakeNextIdStr());
@@ -39,5 +43,21 @@ public class JedisTest {
 		CronUtil.setMatchSecond(true);
 		CronUtil.start();
 		ThreadUtil.safeSleep(6000);
+	}
+
+	/**
+	 * 模拟实时统计在线用户人数
+	 */
+	@Test
+	public void zadd() {
+		Long validityMillis = 30 * 1000L;
+		Long expTime = DateUtil.current() + validityMillis;
+		Long reply10 = jedis.zadd(key, expTime, IdUtil.getSnowflakeNextIdStr());
+		log.info("添加或更新当前uid：{}", reply10);
+		Long reply11 = jedis.zadd(key, expTime, IdUtil.getSnowflakeNextIdStr());
+		log.info("添加或更新当前uid：{}", reply11);
+		Long reply2 = jedis.zremrangeByScore(key, 0, DateUtil.current());
+		log.info("删除小于等于当前时间的记录：{}", reply2);
+		log.info("当前在线人数：{}", jedis.zcard(key));
 	}
 }
