480c2f821f0abae3aec9653c3c2b693fc788509d
任黎明(10021235) <renlm-a@glodon.com>
2022-04-20 13:17:55 +0800

    死信队列

--- src/main/java/cn/renlm/graph/amqp/DeadLetterConfig.java
+++ src/main/java/cn/renlm/graph/amqp/DeadLetterConfig.java
@@ -1,15 +1,27 @@
 package cn.renlm.graph.amqp;
 
+import java.util.Date;
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+import com.rabbitmq.client.Channel;
+
+import cn.hutool.core.date.DateUtil;
 import cn.renlm.graph.util.AmqpUtil;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 死信队列
@@ -17,6 +29,8 @@ import cn.renlm.graph.util.AmqpUtil;
  * @author Renlm
  *
  */
+@Slf4j
+@Component
 @Configuration
 public class DeadLetterConfig {
 
@@ -27,6 +41,18 @@ public class DeadLetterConfig {
 	public static final String queue = key + AmqpUtil.Queue;
 
 	public static final String routingKey = queue + AmqpUtil.RoutingKey;
+
+	/**
+	 * 监听队列
+	 * 
+	 * @param message
+	 * @param channel
+	 */
+	@RabbitListener(bindings = {
+			@QueueBinding(value = @Queue(value = DeadLetterConfig.queue, durable = Exchange.TRUE), exchange = @Exchange(value = DeadLetterConfig.exchange, type = ExchangeTypes.DIRECT), key = DeadLetterConfig.routingKey) })
+	public void receiveMessage(Message message, Channel channel) {
+		log.info("当前时间：{}，收到死信队列消息：{}", DateUtil.formatDateTime(new Date()), message);
+	}
 
 	/**
 	 * 声明交换机
@@ -44,7 +70,7 @@ public class DeadLetterConfig {
 	 * @return
 	 */
 	@Bean
-	public Queue deadLetterQueue() {
+	public org.springframework.amqp.core.Queue deadLetterQueue() {
 		return QueueBuilder.durable(queue).build();
 	}
 
