fa262d4df44e354520dbeba27ee4e131bf5a05ff
任黎明(10021235) <renlm-a@glodon.com>
2022-04-20 14:13:02 +0800

    sss

--- src/main/java/cn/renlm/graph/GraphApplication.java
+++ src/main/java/cn/renlm/graph/GraphApplication.java
@@ -2,6 +2,10 @@ package cn.renlm.graph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import cn.hutool.extra.spring.EnableSpringUtil;
 
@@ -11,11 +15,20 @@ import cn.hutool.extra.spring.EnableSpringUtil;
  * @author Renlm
  *
  */
+@EnableAsync
 @EnableSpringUtil
 @SpringBootApplication
 public class GraphApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphApplication.class, args);
+	}
+
+	@Bean
+	public TaskScheduler taskScheduler() {
+		ThreadPoolTaskScheduler scheduling = new ThreadPoolTaskScheduler();
+		scheduling.setPoolSize(255);
+		scheduling.initialize();
+		return scheduling;
 	}
 }
