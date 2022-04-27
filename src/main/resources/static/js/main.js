92452b2ea78a4dadb57590c4b97facb6f1def115
任黎明(10021235) <renlm-a@glodon.com>
2022-04-27 09:54:44 +0800

    sss

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -26,41 +26,6 @@
 			}
 		},
 		/***
-		 * 简单按钮加载效果
-		 */
-		simpleLoad: function (btn, state) {
-            if (state) {
-                btn.children().addClass("fa-spin");
-                btn.contents().last().replaceWith(" Loading");
-            } else {
-                setTimeout(function () {
-                    btn.children().removeClass("fa-spin");
-                    btn.contents().last().replaceWith(" 刷新");
-                }, 300);
-            }
-        },
-		/***
-		 * Toastr通知（成功）
-		 */
-		toastrSuccess: function (message) {
-			toastr.options = {
-				closeButton: true,
-				debug: false,
-				progressBar: true,
-				positionClass: "toast-top-right",
-				onclick: null,
-				showDuration: "400",
-				hideDuration: "1000",
-				timeOut: "7000",
-				extendedTimeOut: "1000",
-				showEasing: "swing",
-				hideEasing: "linear",
-				showMethod: "fadeIn",
-				hideMethod: "fadeOut"
-			};
-			toastr.success(message);
-		},
-		/***
 		 * 修改密码
 		 */
 		editPwd: function() {
