d6eefa72351889b97a46b00d189db203a39ada0d
任黎明(10021235) <renlm-a@glodon.com>
2022-04-27 13:08:41 +0800

    sss

---
+++ src/main/resources/static/MyUI/js/index.js
@@ -0,0 +1,16 @@
+/*
+ * MyUI 1.0.1, a JS UI library.
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.P = global.P || {})));
+}(this, (function (exports) { 'use strict';
+
+var version = "1.0.1";
+
+exports.version = version;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
