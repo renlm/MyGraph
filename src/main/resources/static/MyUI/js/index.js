6faddd39e49cc7553dc6eeb6d273da0045537c3c
任黎明(10021235) <renlm-a@glodon.com>
2022-04-27 13:51:09 +0800

    sss

--- src/main/resources/static/MyUI/js/index.js
+++ src/main/resources/static/MyUI/js/index.js
@@ -9,7 +9,44 @@
 
 var version = "1.0.1";
 
+/**
+ * 生成菜单树
+ */
+function getMenuTree (data) {
+	let subData = []
+    let child = []
+    data.forEach(e => {
+        if (e.levelId === 2) {
+            e.children = e.state === 'closed' ? [] : ''
+            subData.push(e)
+        } else {
+            child.push(e)
+        }
+    });
+	return recursionMenuTree(subData, child);
+}
+function recursionMenuTree(sourceData, children) {
+    let arr = []
+    if (sourceData) {
+        sourceData.forEach((e) => {
+            children.forEach((c) => {
+                if (e.id == c.pid) {
+                    e.key = e.id
+                    c.children = c.state === 'closed' ? [] : ''
+                    if (e.children) e.children.push(c)
+                }
+            })
+            if (e.children) {
+                recursionMenuTree(e.children, children)
+            }
+        })
+        arr = sourceData
+    }
+    return arr
+}
+
 exports.version = version;
+exports.getMenuTree = getMenuTree;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
