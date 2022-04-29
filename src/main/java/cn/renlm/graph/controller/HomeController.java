4e3391739a6d73a2b51ae0badcd334b8a33ef46e
任黎明(10021235) <renlm-a@glodon.com>
2022-04-29 17:50:34 +0800

    sss

--- src/main/java/cn/renlm/graph/controller/HomeController.java
+++ src/main/java/cn/renlm/graph/controller/HomeController.java
@@ -1,14 +1,20 @@
 package cn.renlm.graph.controller;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.lang.tree.TreeUtil;
 import cn.hutool.json.JSONUtil;
+import cn.renlm.graph.dto.User;
+import cn.renlm.graph.modular.sys.entity.SysResource;
 import cn.renlm.graph.oshi.OshiInfo;
 import cn.renlm.graph.oshi.OshiInfoUtil;
 import cn.renlm.graph.ws.WsUtil;
@@ -22,6 +28,27 @@ import cn.renlm.graph.ws.WsUtil;
 @Controller
 @RequestMapping
 public class HomeController {
+
+	/**
+	 * 主页
+	 * 
+	 * @param authentication
+	 * @param model
+	 * @return
+	 */
+	@GetMapping
+	public String index(Authentication authentication, ModelMap model) {
+		User user = (User) authentication.getPrincipal();
+		List<SysResource> resources = user.getResources();
+		List<Tree<Long>> navGroup = TreeUtil.build(resources, null, (object, treeNode) -> {
+			treeNode.setId(object.getId());
+			treeNode.setName(object.getText());
+			treeNode.setWeight(object.getSort());
+			treeNode.setParentId(object.getPid());
+		});
+		model.put("navGroup", navGroup);
+		return "index";
+	}
 
 	/**
 	 * 在线调试
