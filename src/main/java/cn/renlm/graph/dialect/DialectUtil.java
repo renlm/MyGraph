fe143673a6b563ee25e6d5f2b14a8c9d1ddb2f73
任黎明(10021235) <renlm-a@glodon.com>
2022-04-29 19:42:10 +0800

    sss

--- src/main/java/cn/renlm/graph/dialect/DialectUtil.java
+++ src/main/java/cn/renlm/graph/dialect/DialectUtil.java
@@ -13,7 +13,7 @@ import org.thymeleaf.dialect.IExpressionObjectDialect;
 import org.thymeleaf.expression.IExpressionObjectFactory;
 
 import cn.renlm.graph.modular.sys.service.ISysConstService;
-import cn.renlm.graph.util.DictUtil;
+import cn.renlm.graph.modular.sys.service.ISysDictService;
 import cn.renlm.graph.util.MyConfigProperties;
 
 /**
@@ -27,13 +27,13 @@ public class DialectUtil extends AbstractDialect implements IExpressionObjectDia
 
 	private final Map<String, Object> map = Collections.synchronizedMap(new HashMap<>());
 
-	protected DialectUtil(MyConfigProperties myConfigProperties, ISysConstService iSysConstService, DictUtil dictUtil,
-			Environment environment) {
+	protected DialectUtil(MyConfigProperties myConfigProperties, ISysConstService iSysConstService,
+			ISysDictService iSysDictService, Environment environment) {
 		super(DialectUtil.class.getSimpleName());
-		map.put("MyConfig", myConfigProperties);
-		map.put("SystemConst", iSysConstService);
-		map.put("DictUtil", dictUtil);
-		map.put("ActiveProfiles", environment.getActiveProfiles());
+		map.put("ConfigUtil", myConfigProperties);
+		map.put("ConstUtil", iSysConstService);
+		map.put("DictUtil", iSysDictService);
+		map.put("ProfileUtil", environment.getActiveProfiles());
 	}
 
 	@Override
