20919d81d8b9919670291ba36128a3a39f4494b8
任黎明(10021235) <renlm-a@glodon.com>
2022-04-29 14:52:52 +0800

    sssx

--- src/main/java/cn/renlm/graph/modular/sys/service/impl/SysUserServiceImpl.java
+++ src/main/java/cn/renlm/graph/modular/sys/service/impl/SysUserServiceImpl.java
@@ -1,7 +1,6 @@
 package cn.renlm.graph.modular.sys.service.impl;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,12 +13,12 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.renlm.graph.common.Role;
+import cn.renlm.graph.dto.User;
 import cn.renlm.graph.modular.sys.entity.SysResource;
 import cn.renlm.graph.modular.sys.entity.SysRole;
 import cn.renlm.graph.modular.sys.entity.SysUser;
 import cn.renlm.graph.modular.sys.mapper.SysUserMapper;
 import cn.renlm.graph.modular.sys.service.ISysUserService;
-import cn.renlm.graph.dto.User;
 
 /**
  * <p>
@@ -31,7 +30,7 @@ import cn.renlm.graph.dto.User;
  */
 @Service
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements ISysUserService {
-	
+
 	@Override
 	public User loadUserByUsername(String username) {
 		SysUser sysUser = this.getOne(Wrappers.<SysUser>lambdaQuery().eq(SysUser::getUsername, username));
@@ -40,7 +39,6 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> impl
 		}
 		User user = BeanUtil.copyProperties(sysUser, User.class);
 		List<SysRole> roles = CollUtil.newArrayList();
-		List<Long> sysRoleIds = roles.stream().map(SysRole::getId).collect(Collectors.toList());
 		List<SysResource> resources = CollUtil.newArrayList();
 		List<GrantedAuthority> authorities = CollUtil.newArrayList();
 		if (CollUtil.isNotEmpty(roles)) {
