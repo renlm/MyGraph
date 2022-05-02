85b526488f02d75c7adaf9a78270184a03bb02d3
任黎明(10021235) <renlm-a@glodon.com>
2022-05-02 18:08:31 +0800

    sss

--- src/main/java/cn/renlm/graph/controller/im/ImController.java
+++ src/main/java/cn/renlm/graph/controller/im/ImController.java
@@ -1,7 +1,11 @@
 package cn.renlm.graph.controller.im;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.renlm.graph.response.Layui;
 
 /**
  * 即时通讯
@@ -13,4 +17,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/im")
 public class ImController {
 
+	/**
+	 * 初始化
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/init")
+	public Layui<?> init() {
+		return Layui.success(null);
+	}
+
+	/**
+	 * 查看群员
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/members")
+	public Layui<?> members() {
+		return Layui.success(null);
+	}
+
+	/**
+	 * 上传图片
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/uploadImage")
+	public Layui<?> uploadImage() {
+		return Layui.success(null);
+	}
+
+	/**
+	 * 上传文件
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/uploadFile")
+	public Layui<?> uploadFile() {
+		return Layui.success(null);
+	}
 }
--- src/main/java/cn/renlm/graph/response/Layui.java
+++ src/main/java/cn/renlm/graph/response/Layui.java
@@ -16,10 +16,31 @@ public class Layui<T> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * 响应码
+	 */
 	private int code = 0;
 
+	/**
+	 * 消息
+	 */
 	private String msg;
 
+	/**
+	 * 数据
+	 */
 	private T data;
 
+	/**
+	 * 响应成功
+	 * 
+	 * @param <R>
+	 * @param data
+	 * @return
+	 */
+	public static <R> Layui<R> success(R data) {
+		Layui<R> layui = new Layui<>();
+		layui.setData(data);
+		return layui;
+	}
 }
