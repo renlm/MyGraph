de310472601e3ef8c5ae80093e82dce8e218b5c1
任黎明(10021235) <renlm-a@glodon.com>
2022-05-04 18:25:17 +0800

    sss

--- src/main/java/cn/renlm/graph/amqp/ExcelExportQueueConfig.java
+++ src/main/java/cn/renlm/graph/amqp/ExcelExportQueueConfig.java
@@ -1,13 +1,34 @@
 package cn.renlm.graph.amqp;
 
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import javax.annotation.Resource;
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import cn.hutool.core.codec.Base64;
+import cn.hutool.core.exceptions.ExceptionUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.asymmetric.KeyType;
+import cn.hutool.crypto.asymmetric.RSA;
+import cn.hutool.extra.spring.SpringUtil;
+import cn.renlm.graph.modular.sys.entity.SysFile;
+import cn.renlm.graph.modular.sys.service.ISysFileService;
 
 /**
  * Excel导出队列
@@ -25,6 +46,47 @@ public class ExcelExportQueueConfig {
 	public static final String QUEUE = KEY + AmqpUtil.Queue;
 
 	public static final String ROUTINGKEY = QUEUE + AmqpUtil.RoutingKey;
+
+	@Resource
+	private RSA rsa;
+
+	@Autowired
+	private ISysFileService iSysFileService;
+
+	/**
+	 * 执行表格导出
+	 * 
+	 * @param fileId
+	 */
+	@RabbitListener(bindings = {
+			@QueueBinding(value = @Queue(value = QUEUE, durable = Exchange.TRUE), exchange = @Exchange(value = EXCHANGE, type = ExchangeTypes.DIRECT), key = ROUTINGKEY) })
+	public void receiveMessage(String fileId) {
+		SysFile sysFile = iSysFileService.getById(fileId);
+		// 更新状态
+		sysFile.setStatus(3);
+		sysFile.setUpdatedAt(new Date());
+		iSysFileService.updateById(sysFile);
+		try {
+			String actuator = rsa.decryptStr(Base64.decodeStr(sysFile.getActuator()), KeyType.PublicKey);
+			// 执行任务
+			Class<?> clazz = Class.forName(actuator.substring(0, actuator.lastIndexOf(StrUtil.DOT)));
+			String methodName = actuator.substring(actuator.lastIndexOf(StrUtil.DOT) + 1);
+			Method method = ReflectUtil.getMethod(clazz, methodName, SysFile.class);
+			Object service = SpringUtil.getBean(clazz);
+			ReflectUtil.invoke(service, method, sysFile);
+			// 更新状态
+			sysFile.setStatus(5);
+			sysFile.setUpdatedAt(new Date());
+			iSysFileService.updateById(sysFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// 更新状态
+			sysFile.setStatus(4);
+			sysFile.setUpdatedAt(new Date());
+			sysFile.setMessage(ExceptionUtil.stacktraceToString(e));
+			iSysFileService.updateById(sysFile);
+		}
+	}
 
 	/**
 	 * 声明交换机
