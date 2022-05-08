4ccb291169481e7e86372bd99ed6561663506e8c
任黎明(10021235) <renlm-a@glodon.com>
2022-05-08 16:50:30 +0800

    sss

--- src/main/java/cn/renlm/graph/service/SysAuthAccessService.java
+++ src/main/java/cn/renlm/graph/service/SysAuthAccessService.java
@@ -7,9 +7,16 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.util.StrUtil;
+import cn.renlm.graph.common.TreeState;
 import cn.renlm.graph.modular.sys.dto.SysResourceDto;
+import cn.renlm.graph.modular.sys.entity.SysRole;
 import cn.renlm.graph.modular.sys.service.ISysResourceService;
+import cn.renlm.graph.modular.sys.service.ISysRoleService;
 import cn.renlm.graph.util.TreeExtraUtil;
 
 /**
@@ -20,6 +27,9 @@ import cn.renlm.graph.util.TreeExtraUtil;
  */
 @Service
 public class SysAuthAccessService {
+
+	@Autowired
+	private ISysRoleService iSysRoleService;
 
 	@Autowired
 	private ISysResourceService iSysResourceService;
@@ -33,11 +43,20 @@ public class SysAuthAccessService {
 	 * @return
 	 */
 	public List<Tree<Long>> getTree(String roleId, boolean root, Long pid) {
+		if (StrUtil.isBlank(roleId)) {
+			return CollUtil.newArrayList();
+		}
 		Map<Long, SysResourceDto> authAccessedMap = new LinkedHashMap<>();
 		List<SysResourceDto> authAccessed = iSysResourceService.findListByRole(roleId);
 		authAccessed.forEach(srd -> {
 			authAccessedMap.put(srd.getId(), srd);
 		});
+		if (CollUtil.isEmpty(authAccessed)) {
+			SysRole sysRole = iSysRoleService.getOne(Wrappers.<SysRole>lambdaQuery().eq(SysRole::getRoleId, roleId));
+			if (TreeState.closed.name().equals(sysRole.getState())) {
+				return CollUtil.newArrayList();
+			}
+		}
 		List<Tree<Long>> tree = iSysResourceService.getTree(root, pid);
 		TreeExtraUtil.foreach(tree, node -> {
 			node.putExtra("accessAuth", authAccessedMap.containsKey(node.getId()));
