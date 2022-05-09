678d4a004ab9c8f7f6efad23a627375a36fd0ac4
任黎明(10021235) <renlm-a@glodon.com>
2022-05-09 16:12:13 +0800

    定时任务

---
+++ src/main/java/cn/renlm/graph/controller/qrtz/QuartzController.java
@@ -0,0 +1,16 @@
+package cn.renlm.graph.controller.qrtz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 定时任务
+ * 
+ * @author Renlm
+ *
+ */
+@Controller
+@RequestMapping("/qrtz")
+public class QuartzController {
+
+}
---
+++ src/main/java/cn/renlm/graph/service/QrtzService.java
@@ -0,0 +1,14 @@
+package cn.renlm.graph.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 定时任务
+ * 
+ * @author Renlm
+ *
+ */
+@Service
+public class QrtzService {
+
+}
