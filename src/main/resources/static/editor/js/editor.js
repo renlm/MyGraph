c2cf6efb6756dd43b76be5b5fe06e8500c32669b
任黎明(10021235) <renlm-a@glodon.com>
2022-05-15 11:39:46 +0800

    sss

--- src/main/resources/static/editor/js/editor.js
+++ src/main/resources/static/editor/js/editor.js
@@ -589,11 +589,46 @@
 						frozenColumns: [[
 							{field:'uuid',title:'UUID',checkbox:true},
 							{field:'id',title:'序号',formatter:function(value,row,index){if(value&&index){}return $myDatagrid.datagrid('getRowIndex',row)+1;}},
-							{field:'name',title:'列名',width:150,editor:{type:'textbox',options:{required:true}}},
+							{
+								field: 'name',
+								title: '列名',
+								width: 150,
+								editor: {
+									type: 'textbox',
+									options:{
+										required: true
+									}
+								}
+							},
 							{field:'comment',title:'注释',width:160,editor:{type:'textbox',options:{required:true}}},
 						]],
 						columns: [[
-							{field:'sqlType',title:'数据类型',width:120,formatter:function(value,row,index){if(value&&index){}return JDBC_TYPE[row.sqlType];},editor:{type:'combobox',options:{required:true,editable:true,textField:'label',valueField:'value',panelHeight:350,data:(function(){var data=[];$.each(JDBC_TYPE,function(key,text){data.push({label:text,value:key})});return data;})()}}},
+							{
+								field: 'sqlType',
+								title: '数据类型',
+								width: 120,
+								formatter: function (value, row, index) {
+									if (value && index) { }
+									return JDBC_TYPE[row.sqlType];
+								},
+								editor: {
+									type: 'combobox',
+									options:{
+										required:true,
+										editable:true,
+										textField:'label',
+										valueField:'value',
+										panelHeight:350,
+										data:(function () {
+											var data=[];
+											$.each(JDBC_TYPE, function (key, text) {
+												data.push({ label: text, value: key});
+											});
+											return data;
+										})()
+									}
+								}
+							},
 							{field:'size',title:'长度',width:60,align:'right',editor:{type:'numberspinner',options:{required:false}}},
 							{field:'digit',title:'精度',width:60,align:'right',editor:{type:'numberspinner',options:{required:false}}},
 							{field:'isNullable',title:'是否可为空',width:80,align:'center',formatter:$.yesNoFormatter,editor:{type:'combobox',options:{required:true,editable:false,textField:'label',valueField:'value',panelHeight:70,data:[{label:'是',value:true},{label:'否',value:false}]}}},
