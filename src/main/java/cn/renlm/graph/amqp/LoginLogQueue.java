c517a87f3060b41617eb6552d4c5a3c416403ec0
任黎明(10021235) <renlm-a@glodon.com>
2022-05-16 09:49:42 +0800

    sss

---
+++ src/main/java/cn/renlm/graph/amqp/LoginLogQueue.java
@@ -0,0 +1,84 @@
+package cn.renlm.graph.amqp;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cn.renlm.graph.modular.sys.entity.SysLoginLog;
+import cn.renlm.graph.modular.sys.service.ISysLoginLogService;
+
+/**
+ * 登录日志记录
+ * 
+ * @author Renlm
+ *
+ */
+@Configuration
+public class LoginLogQueue {
+
+	private static final String KEY = "LoginLog";
+
+	public static final String EXCHANGE = KEY + AmqpUtil.Exchange;
+
+	public static final String QUEUE = KEY + AmqpUtil.Queue;
+
+	public static final String ROUTINGKEY = QUEUE + AmqpUtil.RoutingKey;
+
+	@Autowired
+	private ISysLoginLogService iLoginLogService;
+
+	/**
+	 * 写入日志
+	 * 
+	 * @param sysLoginLog
+	 */
+	@RabbitListener(bindings = {
+			@QueueBinding(value = @Queue(value = QUEUE, durable = Exchange.TRUE), exchange = @Exchange(value = EXCHANGE, type = ExchangeTypes.DIRECT), key = ROUTINGKEY) })
+	public void receiveMessage(SysLoginLog sysLoginLog) {
+		iLoginLogService.save(sysLoginLog);
+	}
+
+	/**
+	 * 声明交换机
+	 * 
+	 * @return
+	 */
+	@Bean(name = EXCHANGE)
+	public DirectExchange exchange() {
+		return ExchangeBuilder.directExchange(EXCHANGE).durable(true).build();
+	}
+
+	/**
+	 * 声明队列
+	 * 
+	 * @return
+	 */
+	@Bean(name = QUEUE)
+	public org.springframework.amqp.core.Queue queue() {
+		return QueueBuilder.durable(QUEUE).build();
+	}
+
+	/**
+	 * 绑定队列到交换机
+	 * 
+	 * @param exchange
+	 * @param queue
+	 * @return
+	 */
+	@Bean(name = ROUTINGKEY)
+	public Binding binding(@Qualifier(EXCHANGE) DirectExchange exchange,
+			@Qualifier(QUEUE) org.springframework.amqp.core.Queue queue) {
+		return BindingBuilder.bind(queue).to(exchange).with(ROUTINGKEY);
+	}
+}
--- src/main/java/cn/renlm/graph/security/MyAuthenticationSuccessHandler.java
+++ src/main/java/cn/renlm/graph/security/MyAuthenticationSuccessHandler.java
@@ -1,6 +1,7 @@
 package cn.renlm.graph.security;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import cn.hutool.extra.servlet.ServletUtil;
+import cn.renlm.graph.amqp.AmqpUtil;
+import cn.renlm.graph.amqp.LoginLogQueue;
+import cn.renlm.graph.dto.User;
+import cn.renlm.graph.modular.sys.entity.SysLoginLog;
 
 /**
  * 登录成功处理
@@ -22,7 +29,25 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
+		this.sysLoginLog(request, authentication);
 		super.setAlwaysUseDefaultTargetUrl(true);
 		super.onAuthenticationSuccess(request, response, authentication);
+	}
+
+	/**
+	 * 记录登录日志
+	 * 
+	 * @param request
+	 * @param authentication
+	 */
+	private void sysLoginLog(HttpServletRequest request, Authentication authentication) {
+		User user = (User) authentication.getPrincipal();
+		SysLoginLog sysLoginLog = new SysLoginLog();
+		sysLoginLog.setUserId(user.getUserId());
+		sysLoginLog.setUsername(user.getUsername());
+		sysLoginLog.setNickname(user.getNickname());
+		sysLoginLog.setClientIp(ServletUtil.getClientIPByHeader(request));
+		sysLoginLog.setLoginTime(new Date());
+		AmqpUtil.createQueue(LoginLogQueue.EXCHANGE, LoginLogQueue.ROUTINGKEY, sysLoginLog);
 	}
 }
