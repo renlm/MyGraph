e04c77bc9318cc73661885a595c8f5be256d3514
任黎明(10021235) <renlm-a@glodon.com>
2022-05-18 09:28:46 +0800

    演示账号重置密码

--- src/main/java/cn/renlm/graph/jobs/DemoResetPasswdJob.java
+++ src/main/java/cn/renlm/graph/jobs/DemoResetPasswdJob.java
@@ -4,11 +4,16 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.quartz.JobExecutionContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 
 import cn.hutool.log.level.Level;
 import cn.renlm.graph.modular.qrtz.JobBean;
 import cn.renlm.graph.modular.qrtz.service.IQrtzLogsService;
+import cn.renlm.graph.modular.sys.entity.SysUser;
+import cn.renlm.graph.modular.sys.service.ISysUserService;
 
 /**
  * 演示账号重置密码（0 0/5 * * * ?）
@@ -19,6 +24,8 @@ import cn.renlm.graph.modular.qrtz.service.IQrtzLogsService;
 @Component
 public class DemoResetPasswdJob extends JobBean {
 
+	private ISysUserService iSysUserService;
+
 	public DemoResetPasswdJob(IQrtzLogsService iQrtzLogsService) {
 		super(iQrtzLogsService);
 	}
@@ -26,6 +33,12 @@ public class DemoResetPasswdJob extends JobBean {
 	@Override
 	public void exec(String batch, AtomicInteger seq, Map<String, Object> params) {
 		JobExecutionContext context = (JobExecutionContext) params.get(CONTEXT_KEY);
-		this.log(context, batch, seq, Level.INFO, "重置密码");
+		String username = "S-renyy";
+		String password = "Aac^123654.";
+		iSysUserService.update(Wrappers.<SysUser>lambdaUpdate().func(wrapper -> {
+			wrapper.set(SysUser::getPassword, new BCryptPasswordEncoder().encode(password));
+			wrapper.eq(SysUser::getUsername, username);
+		}));
+		this.log(context, batch, seq, Level.INFO, username + "，重置密码为：" + password);
 	}
 }
