2e4a717d520576509c122c3cce93f7222a224659
任黎明(10021235) <renlm-a@glodon.com>
2022-06-19 12:19:33 +0800

    md

--- src/main/java/cn/renlm/graph/controller/pub/PubDocController.java
+++ src/main/java/cn/renlm/graph/controller/pub/PubDocController.java
@@ -10,8 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+
 import cn.hutool.core.lang.tree.Tree;
 import cn.renlm.graph.modular.doc.dto.DocCategoryShareDto;
+import cn.renlm.graph.modular.doc.entity.DocCategory;
+import cn.renlm.graph.modular.doc.service.IDocCategoryService;
+import cn.renlm.graph.modular.markdown.entity.Markdown;
+import cn.renlm.graph.modular.markdown.service.IMarkdownService;
 import cn.renlm.graph.service.PubDocService;
 
 /**
@@ -26,6 +32,12 @@ public class PubDocController {
 
 	@Autowired
 	private PubDocService pubDocService;
+
+	@Autowired
+	private IMarkdownService iMarkdownService;
+
+	@Autowired
+	private IDocCategoryService iDocCategoryService;
 
 	/**
 	 * 分享（项目）
@@ -80,9 +92,18 @@ public class PubDocController {
 	 */
 	@GetMapping("/m/{shareUuid}")
 	public String docShareMarkdown(ModelMap model, @PathVariable String shareUuid, String docCategoryUuid) {
+		Markdown markdown = iMarkdownService
+				.getOne(Wrappers.<Markdown>lambdaQuery().eq(Markdown::getUuid, docCategoryUuid));
 		DocCategoryShareDto docCategoryShare = pubDocService.getDocCategoryShare(shareUuid);
+		DocCategory docCategory = iDocCategoryService.getOne(
+				Wrappers.<DocCategory>lambdaQuery().eq(DocCategory::getUuid, docCategoryShare.getDocCategoryUuid()));
+		List<DocCategory> fathers = iDocCategoryService.findFathers(docCategoryShare.getDocProjectUuid(),
+				docCategoryShare.getDocCategoryId());
 		model.put("shareUuid", shareUuid);
+		model.put("markdown", markdown);
 		model.put("docCategoryShare", docCategoryShare);
+		model.put("docCategory", docCategory);
+		model.put("fathers", fathers);
 		return "pub/docShareMarkdown";
 	}
 }
