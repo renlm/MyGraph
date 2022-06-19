9ccf79802c887489e92da9752bea102eeceafe7e
任黎明(10021235) <renlm-a@glodon.com>
2022-06-19 20:11:17 +0800

    md

--- src/main/java/cn/renlm/graph/controller/pub/PubDocController.java
+++ src/main/java/cn/renlm/graph/controller/pub/PubDocController.java
@@ -2,22 +2,28 @@ package cn.renlm.graph.controller.pub;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 
 import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.util.NumberUtil;
+import cn.renlm.graph.dto.DocShareUser;
 import cn.renlm.graph.modular.doc.dto.DocCategoryShareDto;
 import cn.renlm.graph.modular.doc.entity.DocCategory;
 import cn.renlm.graph.modular.doc.service.IDocCategoryService;
 import cn.renlm.graph.modular.markdown.entity.Markdown;
 import cn.renlm.graph.modular.markdown.service.IMarkdownService;
+import cn.renlm.graph.response.Result;
 import cn.renlm.graph.service.PubDocService;
 
 /**
@@ -40,15 +46,32 @@ public class PubDocController {
 	private IDocCategoryService iDocCategoryService;
 
 	/**
+	 * 验证密码
+	 * 
+	 * @param shareUuid
+	 * @param password
+	 * @return
+	 */
+	@ResponseBody
+	@PostMapping("/verifyPassword")
+	public Result<?> verifyPassword(HttpServletRequest request, String shareUuid, String password) {
+		return DocShareUser.verifyPassword(request, shareUuid, password);
+	}
+
+	/**
 	 * 分享（项目）
 	 * 
+	 * @param request
 	 * @param model
 	 * @param shareUuid
 	 * @return
 	 */
 	@GetMapping("/s/{shareUuid}")
-	public String docShare(ModelMap model, @PathVariable String shareUuid) {
+	public String docShare(HttpServletRequest request, ModelMap model, @PathVariable String shareUuid) {
 		DocCategoryShareDto docCategoryShare = pubDocService.getDocCategoryShare(shareUuid);
+		if (NumberUtil.equals(docCategoryShare.getShareType(), 2) && DocShareUser.getInfo(request) == null) {
+			return "pub/docSharePasswd";
+		}
 		model.put("shareUuid", shareUuid);
 		model.put("docCategoryShare", docCategoryShare);
 		return "pub/docShare";
@@ -57,13 +80,17 @@ public class PubDocController {
 	/**
 	 * 分享（分类）
 	 * 
+	 * @param request
 	 * @param model
 	 * @param shareUuid
 	 * @return
 	 */
 	@GetMapping("/c/{shareUuid}")
-	public String docShareCategory(ModelMap model, @PathVariable String shareUuid) {
+	public String docShareCategory(HttpServletRequest request, ModelMap model, @PathVariable String shareUuid) {
 		DocCategoryShareDto docCategoryShare = pubDocService.getDocCategoryShare(shareUuid);
+		if (NumberUtil.equals(docCategoryShare.getShareType(), 2) && DocShareUser.getInfo(request) == null) {
+			return "pub/docSharePasswd";
+		}
 		model.put("shareUuid", shareUuid);
 		model.put("docCategoryShare", docCategoryShare);
 		return "pub/docShareCategory";
@@ -85,15 +112,20 @@ public class PubDocController {
 	/**
 	 * 分享（文档）
 	 * 
+	 * @param request
 	 * @param model
 	 * @param shareUuid
 	 * @param uuid
 	 * @return
 	 */
 	@GetMapping("/m/{shareUuid}")
-	public String docShareMarkdown(ModelMap model, @PathVariable String shareUuid, String uuid) {
-		Markdown markdown = iMarkdownService.getOne(Wrappers.<Markdown>lambdaQuery().eq(Markdown::getUuid, uuid));
+	public String docShareMarkdown(HttpServletRequest request, ModelMap model, @PathVariable String shareUuid,
+			String uuid) {
 		DocCategoryShareDto docCategoryShare = pubDocService.getDocCategoryShare(shareUuid);
+		if (NumberUtil.equals(docCategoryShare.getShareType(), 2) && DocShareUser.getInfo(request) == null) {
+			return "pub/docSharePasswd";
+		}
+		Markdown markdown = iMarkdownService.getOne(Wrappers.<Markdown>lambdaQuery().eq(Markdown::getUuid, uuid));
 		DocCategory docCategory = iDocCategoryService
 				.getOne(Wrappers.<DocCategory>lambdaQuery().eq(DocCategory::getUuid, uuid));
 		List<DocCategory> fathers = iDocCategoryService.findFathers(docCategoryShare.getDocProjectUuid(),
--- src/main/java/cn/renlm/graph/dto/DocShareUser.java
+++ src/main/java/cn/renlm/graph/dto/DocShareUser.java
@@ -2,6 +2,9 @@ package cn.renlm.graph.dto;
 
 import java.io.Serializable;
 
+import javax.servlet.http.HttpServletRequest;
+
+import cn.renlm.graph.response.Result;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -22,4 +25,25 @@ public class DocShareUser implements Serializable {
 	 */
 	private String shareUuid;
 
+	/**
+	 * 验证密码
+	 * 
+	 * @param request
+	 * @param shareUuid
+	 * @param password
+	 * @return
+	 */
+	public static final Result<?> verifyPassword(HttpServletRequest request, String shareUuid, String password) {
+		return Result.success();
+	}
+
+	/**
+	 * 获取认证用户信息
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static final DocShareUser getInfo(HttpServletRequest request) {
+		return null;
+	}
 }
