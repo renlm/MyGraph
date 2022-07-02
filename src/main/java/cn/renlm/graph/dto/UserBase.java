8b19bfcdf7596f9fb432109a40c79dd3bf1f2116
任黎明(10021235) <renlm-a@glodon.com>
2022-07-02 14:40:29 +0800

    用户基本信息

--- src/main/java/cn/renlm/graph/dto/UserBase.java
+++ src/main/java/cn/renlm/graph/dto/UserBase.java
@@ -1,9 +1,11 @@
 package cn.renlm.graph.dto;
 
+import java.io.Serializable;
+import java.util.Date;
+
 import cn.hutool.core.bean.BeanUtil;
 import cn.renlm.graph.modular.sys.entity.SysUser;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 /**
@@ -14,27 +16,75 @@ import lombok.experimental.Accessors;
  */
 @Data
 @Accessors(chain = true)
-@EqualsAndHashCode(callSuper = true)
-public class UserBase extends SysUser {
+public class UserBase implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 会话ID
+	 * 会话标识
 	 */
-	private String SESSION;
+	private String ticket;
+
+	/**
+	 * 用户ID
+	 */
+	private String userId;
+
+	/**
+	 * 登录账号
+	 */
+	private String username;
+
+	/**
+	 * 昵称
+	 */
+	private String nickname;
+
+	/**
+	 * 真实姓名
+	 */
+	private String realname;
+
+	/**
+	 * 出生日期
+	 */
+	private Date birthday;
+
+	/**
+	 * 性别，M：男，F：女
+	 */
+	private String sex;
+
+	/**
+	 * 手机号码
+	 */
+	private String mobile;
+
+	/**
+	 * 邮箱地址
+	 */
+	private String email;
+
+	/**
+	 * 个性签名
+	 */
+	private String sign;
+
+	/**
+	 * 头像
+	 */
+	private String avatar;
 
 	/**
 	 * 封装
 	 * 
 	 * @param user
-	 * @param SESSION
+	 * @param ticket
 	 * @return
 	 */
-	public static final UserBase of(SysUser user, String SESSION) {
+	public static final UserBase of(SysUser user, String ticket) {
 		UserBase userBase = BeanUtil.copyProperties(user, UserBase.class);
-		userBase.setSESSION(SESSION);
-		userBase.setPassword(null);
+		userBase.setTicket(ticket);
 		return userBase;
 	}
 }
