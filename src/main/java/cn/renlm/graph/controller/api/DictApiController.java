d8b276edf86f5f3b8856d667e040eea8fc3fcccf
任黎明(10021235) <renlm-a@glodon.com>
2022-07-04 10:00:21 +0800

    字典

--- src/main/java/cn/renlm/graph/controller/api/DictApiController.java
+++ src/main/java/cn/renlm/graph/controller/api/DictApiController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.renlm.graph.modular.sys.entity.SysDict;
 import cn.renlm.graph.modular.sys.service.ISysDictService;
@@ -28,6 +31,26 @@ public class DictApiController {
 	private ISysDictService iSysDictService;
 
 	/**
+	 * 获取子集列表
+	 * 
+	 * @param uuid
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/getChildren")
+	public List<SysDict> getChildren(String uuid) {
+		if (StrUtil.isBlank(uuid)) {
+			return CollUtil.newArrayList();
+		}
+		SysDict sysDict = iSysDictService.getOne(Wrappers.<SysDict>lambdaQuery().eq(SysDict::getUuid, uuid));
+		if (sysDict == null) {
+			return CollUtil.newArrayList();
+		} else {
+			return iSysDictService.findListByPid(sysDict.getId());
+		}
+	}
+
+	/**
 	 * 获取下级列表
 	 * 
 	 * @param pcodes
@@ -37,6 +60,9 @@ public class DictApiController {
 	@GetMapping("/getItem")
 	public List<SysDict> getItem(String pcodes) {
 		String[] codePaths = StrUtil.splitToArray(pcodes, StrUtil.COMMA);
+		if (ArrayUtil.isEmpty(codePaths)) {
+			return CollUtil.newArrayList();
+		}
 		List<SysDict> parents = iSysDictService.findListByPath(codePaths);
 		if (CollUtil.isEmpty(parents)) {
 			return CollUtil.newArrayList();
@@ -56,6 +82,9 @@ public class DictApiController {
 	@GetMapping("/getTree")
 	public List<Tree<Long>> getTree(String pcodes) {
 		String[] codePaths = StrUtil.splitToArray(pcodes, StrUtil.COMMA);
+		if (ArrayUtil.isEmpty(codePaths)) {
+			return CollUtil.newArrayList();
+		}
 		List<Tree<Long>> tree = iSysDictService.getTree(codePaths);
 		return tree;
 	}
