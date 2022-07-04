77f93d9812aab86e4a37ed893e4aad0ff355e413
任黎明(10021235) <renlm-a@glodon.com>
2022-07-04 15:46:19 +0800

    登录

--- src/main/java/cn/renlm/graph/security/MyAuthenticationFailureHandler.java
+++ src/main/java/cn/renlm/graph/security/MyAuthenticationFailureHandler.java
@@ -1,14 +1,24 @@
 package cn.renlm.graph.security;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.ContentType;
+import cn.renlm.graph.response.Result;
+import lombok.Cleanup;
 
 /**
  * 登录失败处理
@@ -19,9 +29,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException exception) throws IOException, ServletException {
-		super.onAuthenticationFailure(request, response, exception);
+		String contentType = request.getContentType();
+		if (StrUtil.equals(ContentType.JSON.getValue(), contentType)) {
+			response.setContentType("application/javascript;charset=" + request.getCharacterEncoding());
+			@Cleanup
+			PrintWriter out = response.getWriter();
+			Result<?> result = Result.of(HttpStatus.BAD_REQUEST, exception.getMessage());
+			out.write(objectMapper.writeValueAsString(result));
+		} else {
+			super.onAuthenticationFailure(request, response, exception);
+		}
 	}
 }
