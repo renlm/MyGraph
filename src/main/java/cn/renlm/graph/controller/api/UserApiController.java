7ddc47945352dbed17f8e99a39042ae70a811604
任黎明(10021235) <renlm-a@glodon.com>
2022-07-07 15:09:58 +0800

    获取当前会话秘钥（AES/CBC/PKCS5Padding）

--- src/main/java/cn/renlm/graph/controller/api/UserApiController.java
+++ src/main/java/cn/renlm/graph/controller/api/UserApiController.java
@@ -24,7 +24,7 @@ import cn.renlm.graph.util.SessionUtil;
 public class UserApiController {
 
 	/**
-	 * 获取会话秘钥（AES/CBC/PKCS5Padding）
+	 * 获取当前会话秘钥（AES/CBC/PKCS5Padding）
 	 * 
 	 * @param request
 	 * @return
