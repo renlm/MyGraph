065b2afda32bad6002a17becc0acb10325a44384
任黎明(10021235) <renlm-a@glodon.com>
2022-09-07 17:27:42 +0800

    md

--- src/main/resources/schema/data.sql
+++ src/main/resources/schema/data.sql
@@ -5038,8 +5038,8 @@ INSERT INTO markdown (id,uuid,name,version,content,graph_uuid,graph_version,data
 分享文档：<a href="https://mygraph.renlm.cn/pub/doc/s/D32C841B469C4ECA86F936238A6D2118" target="_blank"> 系统手册 </a>，查看密码：123654  
 
 ## 源码地址 
-<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://gitee.com/renlm/MyGraph.git</a>  
-<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://github.com/renlm/MyGraph.git</a>  
+<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://<span></span>gitee.com/renlm/MyGraph.git</a>  
+<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://<span></span>github.com/renlm/MyGraph.git</a>  
 
 ## 系统体验
 体验地址：<a href="https://mygraph.renlm.cn/" target="_blank">https://<span></span>mygraph.renlm.cn/</a>  
@@ -10337,8 +10337,8 @@ INSERT INTO markdown (id,uuid,name,version,content,graph_uuid,graph_version,data
 集成语法增强版的Markdown文档、在线画图、数据库ER模型设计、在线数据表格，支持团队协作、版本控制与分享。  
 
 ## 源码地址 
-<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://gitee.com/renlm/MyGraph.git</a>  
-<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://github.com/renlm/MyGraph.git</a>  
+<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://<span></span>gitee.com/renlm/MyGraph.git</a>  
+<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://<span></span>github.com/renlm/MyGraph.git</a>  
 
 ## 系统体验
 体验地址：<a href="https://mygraph.renlm.cn/" target="_blank">https://<span></span>mygraph.renlm.cn/</a>  
@@ -15648,8 +15648,8 @@ INSERT INTO markdown_history (history_id,change_label,operate_at,operator_user_i
 分享文档：<a href="https://mygraph.renlm.cn/pub/doc/s/D32C841B469C4ECA86F936238A6D2118" target="_blank"> 系统手册 </a>，查看密码：123654  
 
 ## 源码地址 
-<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://gitee.com/renlm/MyGraph.git</a>  
-<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://github.com/renlm/MyGraph.git</a>  
+<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://<span></span>gitee.com/renlm/MyGraph.git</a>  
+<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://<span></span>github.com/renlm/MyGraph.git</a>  
 
 ## 系统体验
 体验地址：<a href="https://mygraph.renlm.cn/" target="_blank">https://<span></span>mygraph.renlm.cn/</a>  
@@ -20969,8 +20969,8 @@ INSERT INTO markdown_history (history_id,change_label,operate_at,operator_user_i
 分享文档：<a href="https://mygraph.renlm.cn/pub/doc/s/D32C841B469C4ECA86F936238A6D2118" target="_blank"> 系统手册 </a>，查看密码：123654  
 
 ## 源码地址 
-<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://gitee.com/renlm/MyGraph.git</a>  
-<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://github.com/renlm/MyGraph.git</a>  
+<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://<span></span>gitee.com/renlm/MyGraph.git</a>  
+<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://<span></span>github.com/renlm/MyGraph.git</a>  
 
 ## 系统体验
 体验地址：<a href="https://mygraph.renlm.cn/" target="_blank">https://<span></span>mygraph.renlm.cn/</a>  
--- 帮助文档/使用手册/1.系统简介.md
+++ 帮助文档/使用手册/1.系统简介.md
@@ -13,8 +13,8 @@
 分享文档：<a href="https://mygraph.renlm.cn/pub/doc/s/D32C841B469C4ECA86F936238A6D2118" target="_blank"> 系统手册 </a>，查看密码：123654  
 
 ## 源码地址 
-<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://gitee.com/renlm/MyGraph.git</a>  
-<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://github.com/renlm/MyGraph.git</a>  
+<a href="https://gitee.com/renlm/MyGraph.git" target="_blank">https://<span></span>gitee.com/renlm/MyGraph.git</a>  
+<a href="https://github.com/renlm/MyGraph.git" target="_blank">https://<span></span>github.com/renlm/MyGraph.git</a>  
 
 ## 系统体验
 体验地址：<a href="https://mygraph.renlm.cn/" target="_blank">https://<span></span>mygraph.renlm.cn/</a>  
