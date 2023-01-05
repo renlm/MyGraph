9f8361422aab420e5729f28ff28053fa53689931
任黎明(10021235) <renlm-a@glodon.com>
2023-01-05 17:19:58 +0800

    up

---
+++ src/main/resources/db/schema/oauth2.sql
@@ -0,0 +1,6 @@
+-- org/springframework/security/oauth2/server/authorization/oauth2-authorization-schema.sql
+DROP TABLE IF EXISTS oauth2_authorization;
+-- org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql
+DROP TABLE IF EXISTS oauth2_authorization_consent;
+-- org/springframework/security/oauth2/server/authorization/client/oauth2-registered-client-schema.sql
+DROP TABLE IF EXISTS oauth2_registered_client;
