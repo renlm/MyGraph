96acdfef6bd77afaed0fa753574c9bd277556443
任黎明(10021235) <renlm-a@glodon.com>
2023-01-06 09:58:15 +0800

    up

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM registry.cn-hangzhou.aliyuncs.com/rlm/tomcat10.1.1-jre17
-
-COPY target/ROOT.war /usr/local/tomcat/webapps
-
-ENV TINI_VERSION v0.19.0
-ADD https://renlm.cn/download/tini/${TINI_VERSION}/tini /tini
-RUN chmod +x /tini
-ENTRYPOINT ["/tini", "--"]
-
-CMD ["/bin/bash", "-c", "/usr/local/tomcat/bin/catalina.sh start && tail -f /usr/local/tomcat/logs/catalina.out"]
- 
+FROM openjdk:17-jdk-alpine
+ADD target/auth-server-0.0.1.jar app.jar
+VOLUME /tmp
+RUN touch /app.jar
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
+  && apk update \
+  && apk upgrade \
+  && apk --no-cache add ttf-dejavu fontconfig
 EXPOSE 8080
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/cn/renlm/graph/GraphApplication.java
+++ src/main/java/cn/renlm/graph/GraphApplication.java
@@ -4,8 +4,6 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
@@ -30,12 +28,7 @@ import cn.hutool.extra.spring.EnableSpringUtil;
 @EnableScheduling
 @EnableSpringUtil
 @SpringBootApplication
-public class GraphApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GraphApplication.class);
-	}
+public class GraphApplication {
 
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(GraphApplication.class);
