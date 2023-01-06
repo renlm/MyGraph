680b5909adca71e23d9d465bf22b2a5e460e46e3
任黎明(10021235) <renlm-a@glodon.com>
2023-01-06 16:17:01 +0800

    up

--- src/main/java/cn/renlm/graph/jobs/DemoResetPasswdJob.java
+++ src/main/java/cn/renlm/graph/jobs/DemoResetPasswdJob.java
@@ -1,5 +1,6 @@
 package cn.renlm.graph.jobs;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONUtil;
 import cn.hutool.log.level.Level;
 import cn.renlm.graph.modular.qrtz.JobBean;
 import cn.renlm.graph.modular.qrtz.service.IQrtzLogsService;
@@ -27,7 +30,7 @@ public class DemoResetPasswdJob extends JobBean {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private ISysUserService iSysUserService;
 
@@ -38,12 +41,13 @@ public class DemoResetPasswdJob extends JobBean {
 	@Override
 	public void exec(String batch, AtomicInteger seq, Map<String, Object> params) {
 		JobExecutionContext context = (JobExecutionContext) params.get(CONTEXT_KEY);
-		String username = "S-renyy";
+		List<String> usernames = CollUtil.newArrayList("S-linghc", "S-renyy");
 		String password = "123654";
 		iSysUserService.update(Wrappers.<SysUser>lambdaUpdate().func(wrapper -> {
 			wrapper.set(SysUser::getPassword, passwordEncoder.encode(password));
-			wrapper.eq(SysUser::getUsername, username);
+			wrapper.in(SysUser::getUsername, usernames);
 		}));
-		this.log(context, batch, seq, Level.INFO, username + "，重置密码为：" + password);
+		this.log(context, batch, seq, Level.INFO, JSONUtil.toJsonStr(usernames) + "，重置密码为：" + password);
 	}
+
 }
