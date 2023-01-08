25132fd91216739667a3809f1ff30b666b837c11
任黎明(10021235) <renlm-a@glodon.com>
2023-01-08 21:40:59 +0800

    up

--- src/main/java/cn/renlm/graph/modular/oauth2/entity/Oauth2RegisteredClient.java
+++ src/main/java/cn/renlm/graph/modular/oauth2/entity/Oauth2RegisteredClient.java
@@ -26,7 +26,7 @@ public class Oauth2RegisteredClient implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId(value = "id", type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
     private String id;
 
     @TableField("client_id")
