34d2c75d9341aa99628285629d656386e350cf2c
renlm-a <renlm-a@glodon.com>
2024-09-07 18:10:19 +0800

    up

--- src/test/java/cn/renlm/mygraph/OAuth2ClientTest.java
+++ src/test/java/cn/renlm/mygraph/OAuth2ClientTest.java
@@ -17,6 +17,7 @@ import org.springframework.security.oauth2.server.authorization.settings.TokenSe
 import org.springframework.test.context.ActiveProfiles;
 
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
 
 /**
  * 注册客户端
@@ -33,31 +34,32 @@ public class OAuth2ClientTest {
 
 	@Test
 	public void test() {
+		String clientId = "oidc-client";
 		registeredClientRepository.save(RegisteredClient
-				.withId(IdUtil.simpleUUID().toUpperCase())
-				.clientId("test-client")
-				.clientSecret("{noop}secret")
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-				.redirectUri("http://127.0.0.1:7002/login/oauth2/code/test-client")
-				.scope(OidcScopes.OPENID)
-				.scope(OidcScopes.PROFILE)
-				.clientSettings(ClientSettings.builder()
-					.requireProofKey(false)
-					.requireAuthorizationConsent(false)
-					.build())
-				.tokenSettings(TokenSettings.builder()
-					.authorizationCodeTimeToLive(Duration.ofMinutes(5))
-					.accessTokenTimeToLive(Duration.ofMinutes(5))
-					.accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
-					.reuseRefreshTokens(true)
-					.refreshTokenTimeToLive(Duration.ofMinutes(60))
-					.idTokenSignatureAlgorithm(SignatureAlgorithm.RS256)
-					.build())
-				.build());
+			.withId(IdUtil.simpleUUID().toUpperCase())
+			.clientId(clientId)
+			.clientSecret("{noop}secret")
+			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+			.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+			.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+			.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+			.redirectUri(StrUtil.format("http://localhost/login/oauth2/code/{}", clientId))
+			.scope(OidcScopes.OPENID)
+			.scope(OidcScopes.PROFILE)
+			.clientSettings(ClientSettings.builder()
+				.requireProofKey(false)
+				.requireAuthorizationConsent(false)
+				.build())
+			.tokenSettings(TokenSettings.builder()
+				.authorizationCodeTimeToLive(Duration.ofMinutes(5))
+				.accessTokenTimeToLive(Duration.ofMinutes(5))
+				.accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+				.reuseRefreshTokens(true)
+				.refreshTokenTimeToLive(Duration.ofMinutes(60))
+				.idTokenSignatureAlgorithm(SignatureAlgorithm.RS256)
+				.build())
+			.build());
 	}
 
 }
